cbe7ba685b3927d37e1b17517c1f24190a70d7bf
Armanlone <armanlonedev@gmail.com>
2025-01-30 12:11:51 +0800

    FadeIn/.FadeOut animation for "Copied!" text

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,15 +1,37 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
 export class HomepageComponent {
   showCopiedTextInCapitalEnye : boolean = false;
   showCopiedTextInSmallEnye : boolean = false;
+  
+  capitalEnyeButtonState !: string;
+  capitalEnyePressed(text: string) : void {
+    this.capitalEnyeButtonState = "pressed";
+    this.copyToClipboard(text);
+    setTimeout(() => {
+      this.capitalEnyeButtonState = "normal";
+      this.showCopiedTextInCapitalEnye = false;
+    }, 2500);
+  }
+
+  smallEnyeButtonState !: string;
+  smallEnyePressed(text: string) : void {
+    this.smallEnyeButtonState = "pressed";
+    this.copyToClipboard(text);
+    setTimeout(() => {
+      this.smallEnyeButtonState = "normal";
+      this.showCopiedTextInSmallEnye = false;
+    }, 2500);
+  }
+
   copyToClipboard(text: string) : void {
     navigator.clipboard.writeText(text)
       .then(() => {
@@ -22,9 +44,8 @@ export class HomepageComponent {
         }
 
         setTimeout(() => {
-          this.showCopiedTextInCapitalEnye = false;
           this.showCopiedTextInSmallEnye = false;
-        }, 2000);
+        }, 2500);
       })
       .catch((err) => {alert(err)});
   }
